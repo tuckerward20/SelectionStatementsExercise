9cfbf946bba76c0b7df6aa496dc51297d05b1197
tuckerward20 <tuckerward20@gmail.com>
2021-01-25 14:40:01 -0600

    Firstupload

---
+++ SelectionStatementExerciseTry2/SelectionStatementExerciseTry2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SelectionStatementExerciseTry2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        }
+    }
+}
